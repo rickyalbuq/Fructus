8397b1a8107e946d80218255be9954b50694a2bf
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2024-06-29 08:05:18 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Henrique Albuquerque on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
